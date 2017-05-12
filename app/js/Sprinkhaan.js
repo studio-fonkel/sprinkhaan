42dc937000c7a50d61c0fe14cd59bec290d2c5f0
Daniel Beeke <daniel@studiofonkel.nl>
2017-05-12 23:21:42 +0200

    fixing bug with heights

--- app/js/Sprinkhaan.js
+++ app/js/Sprinkhaan.js
@@ -52,7 +52,7 @@ class Sprinkhaan extends EventEmitter {
         });
 
         this.elements['content'].style.marginTop = this.elements['header.is-not-sticky'].clientHeight + 'px';
-        this.element.style.height = Math.min(window.innerHeight, this.elements['content-wrapper'].clientHeight + this.elements['media'].clientHeight + 'px');
+        this.element.style.height = Math.min(this.element.clientHeight, this.elements['content-wrapper'].clientHeight + this.elements['media'].clientHeight) + 'px';
         this.createAnimations();
         this.attachEventHandlers();
     }
