ca6ab667125f411844cf5183dab14597a646fc8f
Daniel Beeke <daniel@studiofonkel.nl>
2017-05-14 11:28:10 +0200

    debounce to es6

--- app/js/Sprinkhaan.js
+++ app/js/Sprinkhaan.js
@@ -411,14 +411,14 @@ class Sprinkhaan extends EventEmitter {
     }
 
     debounce(func, wait, immediate) {
-        var timeout;
+        let timeout;
         return function() {
-            var context = this, args = arguments;
-            var later = function() {
+            let context = this, args = arguments;
+            let later = function() {
                 timeout = null;
                 if (!immediate) func.apply(context, args);
             };
-            var callNow = immediate && !timeout;
+            let callNow = immediate && !timeout;
             clearTimeout(timeout);
             timeout = setTimeout(later, wait);
             if (callNow) func.apply(context, args);
